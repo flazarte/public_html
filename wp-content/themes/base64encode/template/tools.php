8b0e7be6cdd397169c42aa6a4fa2801854ce6868
flazarte <ferdinandlazarte2017@gmail.com>
2021-01-03 20:46:47 +0800

    Tools template modif

--- wp-content/themes/base64encode/template/tools.php
+++ wp-content/themes/base64encode/template/tools.php
@@ -1,20 +1,4 @@
-<!-- <div class="p-4">
-        <h4 class="font-italic">Tools</h4>
-        <ol class="list-unstyled mb-0">
-          <li><a href="#">March 2014</a></li>
-          <li><a href="#">February 2014</a></li>
-          <li><a href="#">January 2014</a></li>
-          <li><a href="#">December 2013</a></li>
-          <li><a href="#">November 2013</a></li>
-          <li><a href="#">October 2013</a></li>
-          <li><a href="#">September 2013</a></li>
-          <li><a href="#">August 2013</a></li>
-          <li><a href="#">July 2013</a></li>
-          <li><a href="#">June 2013</a></li>
-          <li><a href="#">May 2013</a></li>
-          <li><a href="#">April 2013</a></li>
-        </ol>
-      </div> -->
+
       <div class="card">
 	<article class="card-group-item">
 		<header class="card-header"><h6 class="title">Popular Tools</h6></header>
